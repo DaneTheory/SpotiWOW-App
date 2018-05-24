a5103bba1bedb1e1bb9cc4dce074d23cbbded7fb
Branden Dane <danetheory@gmail.com>
2018-05-24 08:36:29 -0400

    initial code push
    initial push. Includes code base for API server and client UI. Added
    production build scripts.

---
+++ client/tools/srcServer.js
@@ -0,0 +1,44 @@
+import browserSync from 'browser-sync';
+import historyApiFallback from 'connect-history-api-fallback';
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import config from '../webpack.config.dev';
+import proxy from 'http-proxy-middleware';
+
+const apiProxy = proxy('/api', {
+    target: 'http://localhost:9999',
+    changeOrigin: false   // for vhosted sites
+});
+
+const bundler = webpack(config);
+
+browserSync({
+  port: 9888,
+  ui: false,
+  server: {
+    baseDir: 'src',
+    middleware: [
+      historyApiFallback(),
+      apiProxy,
+      webpackDevMiddleware(bundler, {
+        publicPath: config.output.publicPath,
+        noInfo: false,
+        quiet: false,
+        stats: {
+          assets: false,
+          colors: true,
+          version: false,
+          hash: false,
+          timings: false,
+          chunks: false,
+          chunkModules: false
+        },
+      }),
+      webpackHotMiddleware(bundler),
+    ]
+  },
+  files: [
+    'src/*.html'
+  ]
+});
---
+++ configs/port.js
@@ -0,0 +1,2 @@
+let portNumber = 9999;
+export const Port = process.env.PORT || portNumber;
